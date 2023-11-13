c996c4f9c00ef56a0ac084ee643ab7f18179925b
ahmed Qasem <abok2ssem14@gmail.com>
2023-11-13 16:39:30 +0200

    Refactor: apply single responsibility principle

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,9 +1,8 @@
-import { IssueStatusBadge } from "@/app/components";
 import prisma from "@/prisma/client";
-import { Box, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
+import { Box, Grid } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
-import ReactMarkDown from "react-markdown";
-import EditIssueButton from "./edit/EditIssueButton";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
 
 interface Props {
   params: { id: string };
@@ -18,14 +17,7 @@ const IssueDetailPage = async ({ params }: Props) => {
   return (
     <Grid columns={{initial: "1" , md: "2"}} gap="5">
       <Box>
-      <Heading>{issue.title}</Heading>
-      <Flex className="space-x-3" my="2">
-        <IssueStatusBadge status={issue.status} />
-        <Text>{issue.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card mt="4" className="pros">
-        <ReactMarkDown>{issue.description}</ReactMarkDown>
-      </Card>
+      <IssueDetails issue={issue}/>
       </Box>
       <Box>
         <EditIssueButton issueId={issue.id}/>
