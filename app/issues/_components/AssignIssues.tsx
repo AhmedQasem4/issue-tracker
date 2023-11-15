35bcb09d13941996df8c31e71fd7a0a1f66ba48e
ahmed Qasem <abok2ssem14@gmail.com>
2023-11-15 17:04:37 +0200

    Fetch users using react query

--- app/issues/_components/AssignIssues.tsx
+++ app/issues/_components/AssignIssues.tsx
@@ -1,17 +1,31 @@
-'use client';
+"use client";
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import React from "react";
-
+import {Skeleton} from '@/app/components';
 const AssignIssues = () => {
-  
+  const { data: users, error, isLoading } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000, // 60s
+    retry: 3,    
+  });
+  if(error) return null;
 
+  if(isLoading) return <Skeleton />
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="1">Ahmed Qasem</Select.Item>
+          {users?.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.email}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
