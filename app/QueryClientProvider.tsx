46c65d158dd547000d8b220a9c27bcdbe38d7156
ahmed Qasem <abok2ssem14@gmail.com>
2023-11-15 16:54:07 +0200

    Set up react query

---
+++ app/QueryClientProvider.tsx
@@ -0,0 +1,14 @@
+'use client';
+import {
+  QueryClient,
+  QueryClientProvider as ReactQueryClientProvider,
+} from "@tanstack/react-query";
+import React, { PropsWithChildren } from "react";
+const queryClient = new QueryClient();
+const QueryClientProvider = ({ children }: PropsWithChildren) => {
+  return (
+    <ReactQueryClientProvider client={queryClient}></ReactQueryClientProvider>
+  );
+};
+
+export default QueryClientProvider;
