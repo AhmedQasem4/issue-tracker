db216b3c8dc9c674a67075442ab7d994f6c7e19d
ahmed Qasem <abok2ssem14@gmail.com>
2023-11-17 13:39:01 +0200

    Optimize performance using react cache

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,5 +1,5 @@
 import prisma from "@/prisma/client";
-import { Box, Flex, Grid, Select } from "@radix-ui/themes";
+import { Box, Flex, Grid } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
@@ -7,15 +7,16 @@ import DeleteIssueButton from "../_components/DeleteIssueButton";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import AssignIssues from "../_components/AssignIssues";
-
+import {cache} from 'react';
 interface Props {
   params: { id: string };
 }
 
+const fetchUser = cache((issueId: number) => prisma.issue.findUnique({where: {id: issueId}}));
+
 const IssueDetailPage = async ({ params }: Props) => {
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+
+  const issue = await fetchUser(parseInt(params.id));
 
   const sessiton = await getServerSession(authOptions);
 
@@ -37,7 +38,7 @@ const IssueDetailPage = async ({ params }: Props) => {
 };
 
 export async function generateMetadata({ params }: Props) {
-  const issue = await prisma.issue.findUnique({ where: { id: parseInt(params.id) }});
+  const issue = await fetchUser(parseInt(params.id));
 
   return {
     title: issue?.title,
