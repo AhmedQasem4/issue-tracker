305fee8e129a44b1e087930886f4c00b98ac99ee
ahmed Qasem <abok2ssem14@gmail.com>
2023-11-17 08:52:43 +0200

    Sort issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -5,15 +5,15 @@ import NewIssueButton from "./NewIssueButton";
 import IssueStatusFilter from "./_components/IssueStatusFilter";
 import { Issue, Status } from "@prisma/client";
 import Link from "next/link";
-import {ArrowUpIcon} from '@radix-ui/react-icons'
+import { ArrowUpIcon } from "@radix-ui/react-icons";
 
 interface Props {
-  searchParams: { status: Status , orderBy: keyof Issue };
+  searchParams: { status: Status; orderBy: keyof Issue };
 }
 
 const columns: { label: string; value: keyof Issue; className?: string }[] = [
   { label: "Issue", value: "title" },
-  { label: "Status", value: "status" , className: "hidden md:table-cell"},
+  { label: "Status", value: "status", className: "hidden md:table-cell" },
   { label: "Date", value: "createdAt", className: "hidden md:table-cell" },
 ];
 
@@ -22,10 +22,16 @@ const IssuesPage = async ({ searchParams }: Props) => {
   const status = statueses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
   const issues = await prisma.issue.findMany({
     where: {
       status,
     },
+    orderBy,
   });
   const url = new URLSearchParams();
   return (
@@ -39,13 +45,19 @@ const IssuesPage = async ({ searchParams }: Props) => {
           <Table.Row>
             {columns.map((column) => (
               <Table.ColumnHeaderCell key={column.value}>
-                <Link href={{
-                  query: {
-                    ...searchParams,
-                    orderBy: column.value,
-                  }
-                }}>{column.label}</Link>
-                {searchParams.orderBy === column.value && <ArrowUpIcon className="inline"/>}
+                <Link
+                  href={{
+                    query: {
+                      ...searchParams,
+                      orderBy: column.value,
+                    },
+                  }}
+                >
+                  {column.label}
+                </Link>
+                {searchParams.orderBy === column.value && (
+                  <ArrowUpIcon className="inline" />
+                )}
               </Table.ColumnHeaderCell>
             ))}
           </Table.Row>
