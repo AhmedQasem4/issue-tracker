911bc1fe7982ad45a48bb7d3f4aec17f86a867f1
ahmed Qasem <abok2ssem14@gmail.com>
2023-12-12 04:25:21 +0200

    Re-Setup sentry

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,7 +6,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://29606f28a1f6e3c09e7102f0601e6087@o4506137339035648.ingest.sentry.io/4506241029767168",
+  dsn: "https://b8db25530f40d136e4ecba7e38874e77@o4506137339035648.ingest.sentry.io/4506380377063424",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
